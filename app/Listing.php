c4cbb3ca3460c51ba68887a4fdbf86f3d12c92b3
KakehashiMasato <kakehashi.masato@gmail.com>
2019-11-12 02:52:41 +0000

    create model&table

---
+++ app/Listing.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    protected $fillable = [
+        'title',
+        'user_id',
+        'created_at',
+        'updated_at'
+    ];
+}
